b9892c49951914f383eeb1b879e254c0318716ae
Sayali <sayali.hate@tejgyan.com>
2024-10-18 15:45:30 +0530

    Implemented Singleton design pattern with Redis integration

---
+++ SingletonDesignPattern1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SingletonDesignPattern1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Singleton s1 = Singleton.GetInstance();
+            //Singleton s2 = Singleton.GetInstance();
+
+            //if (s1 != s2)
+            //{
+            //    Console.WriteLine("Not sharing the same resources"); 
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sharing the same resources");
+            //}
+
+            Singleton s1 = Singleton.GetInstance();
+            s1.getDatabaseObject().StringSet("person", "cleverPerson");
+
+            Singleton s2 = Singleton.GetInstance();
+           string value =  s2.getDatabaseObject().StringGet("person");
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ SingletonDesignPattern1/Singleton.cs
@@ -0,0 +1,45 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern1
+{
+    internal class Singleton
+    {
+        //Declare private static field
+        private static Singleton instance;
+
+        private static IConnectionMultiplexer conn;
+        private static IDatabase db;
+
+        //private constructor
+        private Singleton()
+        {
+            conn = ConnectionMultiplexer.Connect("localhost");
+        }
+
+        // GetInstance method which will be accessed from the outside scope
+        // will return instance always when invoked
+        // if first time call create instance
+        // else return  already created
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+
+        // an business logic method that needed
+        public IDatabase getDatabaseObject()
+        {
+            return conn.GetDatabase();
+        }
+    }
+}
+
